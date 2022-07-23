6ca0f07a522289b45abf6413f23775d43bccda41
bobbyrosehag <bobbyrosehag@gmail.com>
2022-07-23 19:11:59 +0200

    Added frontend serving

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import connectDB from "./config/db";
 import router from "./routes/pollRoutes";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.port || 3500;
@@ -12,3 +13,14 @@ app.use(express.json());
 app.use("/api/polls", router);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
